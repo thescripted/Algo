5ffb18bdfe05d2a3d7a33aef5b542cdb70306bbe
Benjamin Kinga <somethingscripted@gmail.com>
2020-04-21 02:55:52 -0500

    initial commit

---
+++ binary-tree.py
@@ -0,0 +1,15 @@
+"""Creating a binary tree object in python"""
+
+class Node:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def PrintTree(self):
+        print(self.data)
+
+if __name__ == "__main__":
+    root = Node(10)
+    root.PrintTree()
